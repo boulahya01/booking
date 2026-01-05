dc9404dd970e6a11f942349cc7a2c99b365f16e5
boulahya01 <102596690+boulahya01@users.noreply.github.com>
2026-01-05 02:35:50 +0100

    chore(root): remove 7 files
    Removed files:\n - check_bookings.js
     - deploy_function.sh
     - insert_pitch.sql
     - test_booking_jobs_trigger.js
     - test_get_columns.js
     - verify-security.sh
     - verify_mobile_menu.sh

--- insert_pitch.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO pitches (name, location, capacity, open_time, close_time)
-VALUES (
-  'Football Pitch',
-  'Downtown Sports Complex',
-  20,
-  '08:00'::TIME,
-  '22:00'::TIME
-)
-ON CONFLICT DO NOTHING;
