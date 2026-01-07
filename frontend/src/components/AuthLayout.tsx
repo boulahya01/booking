7f4e0bc66956c9f029d0d7ae57bbe0a055c96dd8
boulahya01 <102596690+boulahya01@users.noreply.github.com>
2026-01-07 03:28:02 +0100

    AuthLayout: add LanguageDropdown in top-right corner

--- frontend/src/components/AuthLayout.tsx
+++ frontend/src/components/AuthLayout.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { LanguageDropdown } from './LanguageDropdown'
 import './AuthLayout.css'
 
 export const AuthLayout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return (
     <div className="auth-layout">
+      <div className="auth-layout-language-selector">
+        <LanguageDropdown />
+      </div>
       <div className="auth-layout-container">
         {children}
       </div>
